16c08b54075d4c9478d5f71a092a4a95fb2141f3
dinskstroi1217-hub <dinskstroi1217@gmail.com>
2026-05-22 18:20:37 +0300

    deploy: base=/ for custom domain 2026-05-22T15:20:37Z

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kip-frontend/sw.js', { scope: '/kip-frontend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
